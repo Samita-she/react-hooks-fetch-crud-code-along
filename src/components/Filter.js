16a39f0b28c01e46217588adc2c3b54a579cec4f
Sheila Samita <samitasheila1@gmail.com>
2025-04-24 01:21:18 +0300

    performed CRUD actions on arrays in state

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,20 +1,33 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
-function Filter({ category, onCategoryChange }) {
+function Filter({ category, onCategoryChange, categories }) {
   return (
     <div className="Filter">
+      <label htmlFor="category-filter">Filter by category:</label>
       <select
+        id="category-filter"
         name="filter"
         value={category}
         onChange={(e) => onCategoryChange(e.target.value)}
       >
-        <option value="All">Filter by category</option>
-        <option value="Produce">Produce</option>
-        <option value="Dairy">Dairy</option>
-        <option value="Dessert">Dessert</option>
+        <option value="All">All Categories</option>
+        {categories.map((cat) => (
+          <option key={cat} value={cat}>{cat}</option>
+        ))}
       </select>
     </div>
   );
 }
+
+Filter.propTypes = {
+  category: PropTypes.string.isRequired,
+  onCategoryChange: PropTypes.func.isRequired,
+  categories: PropTypes.arrayOf(PropTypes.string)
+};
+
+Filter.defaultProps = {
+  categories: ["Produce", "Dairy", "Dessert"]
+};
 
 export default Filter;
